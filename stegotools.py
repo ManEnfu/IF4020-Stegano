3a4633b3f3f8cd7f726ba105a06703e59802153b
BeRay31 <beray.prof.if@gmail.com>
2021-09-30 00:13:35 +0700

    feat: io file

--- stegotools.py
+++ stegotools.py
@@ -1,4 +1,6 @@
 import sys
+import util
+import rc4
 from PyQt5 import QtCore, QtWidgets
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -7,44 +9,61 @@ class MainWindow(QtWidgets.QMainWindow):
        
         self.media = None
         self.message = b''
+        self.result = None
         self.msgascii = True
+
         self.media_textbox = QtWidgets.QTextEdit(self)
-        self.message_textbox = QtWidgets.QTextEdit(self)
         self.media_textbox.setReadOnly(True)
-        self.message_textbox.setReadOnly(True)
         self.open_media_button = QtWidgets.QPushButton('Open', self)
-        self.save_media_button = QtWidgets.QPushButton('Save', self)
-        # self.media_label = QtWidgets.QLabel('Open File...', self)
+
+        self.message_textbox = QtWidgets.QTextEdit(self)
+        self.message_textbox.setReadOnly(True)
         self.open_message_button = QtWidgets.QPushButton('Open', self)
-        self.save_message_button = QtWidgets.QPushButton('Save', self)
-        # self.message_label = QtWidgets.QLabel('Open File...', self)
+
+        self.result_label = QtWidgets.QLabel("Result")
+        self.result_textbox = QtWidgets.QTextEdit(self)
+        self.result_textbox.setReadOnly(True)
+        self.save_result_button = QtWidgets.QPushButton('Save', self)
+        
         self.key_textbox = QtWidgets.QLineEdit(self)
         self.embed_button = QtWidgets.QPushButton('Embed', self)
         self.extract_button = QtWidgets.QPushButton('Extract', self)
         self.seq_radio = QtWidgets.QRadioButton('Sequential', self)
         self.rand_radio = QtWidgets.QRadioButton('Random', self)
+        # self.media_label = QtWidgets.QLabel('Open File...', self)
+        # self.message_label = QtWidgets.QLabel('Open File...', self)
 
         self.setWindowTitle('StegoTools')
 
         center = QtWidgets.QWidget(self)
         clayout = QtWidgets.QVBoxLayout(self)
+        
         media_button_layout = QtWidgets.QVBoxLayout(self)
         message_button_layout = QtWidgets.QVBoxLayout(self)
+        result_button_layout = QtWidgets.QVBoxLayout(self)
+
         embed_layout = QtWidgets.QVBoxLayout(self)
         extract_layout = QtWidgets.QVBoxLayout(self)
+        result_layout = QtWidgets.QVBoxLayout(self)
+
         top_layout = QtWidgets.QHBoxLayout(self)
         bot_layout = QtWidgets.QHBoxLayout(self)
 
         media_button_layout.addWidget(QtWidgets.QLabel('Media'))
         media_button_layout.addWidget(self.media_textbox)
         media_button_layout.addWidget(self.open_media_button)
-        media_button_layout.addWidget(self.save_media_button)
+
         message_button_layout.addWidget(QtWidgets.QLabel('Message'))
         message_button_layout.addWidget(self.message_textbox)
         message_button_layout.addWidget(self.open_message_button)
-        message_button_layout.addWidget(self.save_message_button)
+        
+        result_button_layout.addWidget(self.result_label)
+        result_button_layout.addWidget(self.result_textbox)
+        result_button_layout.addWidget(self.save_result_button)
+
         top_layout.addLayout(media_button_layout)
         top_layout.addLayout(message_button_layout)
+        top_layout.addLayout(result_button_layout)
 
         # embed_layout.addWidget(QtWidgets.QLabel('Embed'))
         embed_layout.addWidget(QtWidgets.QLabel('Insert Order:'))
@@ -66,20 +85,30 @@ class MainWindow(QtWidgets.QMainWindow):
         
         center.setLayout(clayout)
         self.setCentralWidget(center)
-
         self.seq_radio.click()
 
-    def handle_open_media_button(self):
-        pass
+        self.open_media_button.clicked.connect(self.handle_open_media_button)
+        self.open_message_button.clicked.connect(self.handle_open_message_button)
+        self.save_result_button.clicked.connect(self.handle_save_result_button)
 
-    def handle_save_media_button(self):
-        pass
+    def handle_open_media_button(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', '.')
+        if filename[0] != '':
+            self.media = util.read_file_binary(filename[0])
+            self.media_textbox.setText('Raw Binary Data (' + str(len(self.media)) + 'B)')
     
     def handle_open_message_button(self):
-        pass
+        filename = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', '.')
+        if filename[0] != '':
+            self.message = util.read_file_binary(filename[0])
+            self.message_textbox.setText('Raw Binary Data (' + str(len(self.message)) + 'B)')
 
-    def handle_save_message_button(self):
-        pass
+    def handle_save_result_button(self):
+        if self.result != None:
+            filename = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File', '.')
+            if filename[0] != '':
+                util.write_file_binary(filename[0], self.result)
+
 
     def handle_embed_button(self):
         pass
